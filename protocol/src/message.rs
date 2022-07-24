7558502fea6f3bc43e2405ab78904b6f54ae2461
Olly Swanson <olly.swanson95@gmail.com>
2022-07-24 13:46:49 +0100

    Improve documentaton for `Message`

--- protocol/src/message.rs
+++ protocol/src/message.rs
@@ -1,6 +1,10 @@
-//! BNF for protocol
+//! Message protocol.
+//!
+//! BNF for protocol:
+//!
+//! ```text
 //! Message ::= (Prefix Space)? Command CRLF
-//! Prefix ::= ':' Nickname ; Can be expanded in the future
+//! Prefix ::= ':' Nickname /* Can be expanded in the future */
 //! Command ::= Letter+ Params*
 //! Params ::= (Space Middle)* (Space ':' Trailing)?
 //! Middle ::= NoColonCRLFSpace (':' | NoColonCRLFSpace)*
@@ -8,26 +12,65 @@
 //! NoColonCRLFSpace ::= #x00-#x09 | #x0B-#x0C | #x0E-#x1F | #x21-#x39 | #x3B-#xFF /* No Colon, CR, LF, or Space */
 //! CRLF ::= #x0D #x0A
 //! Nickname ::= ascii_alphabetical
+//! ```
+use std::fmt;
+use std::str::FromStr;
+
 use crate::command::{parse_command, Command};
 use nom::{
-    character::complete::{alpha1, char},
-    combinator::{map, opt},
+    character::complete::{alpha1, char, crlf},
+    combinator::{complete, map, opt},
     sequence::{pair, preceded, terminated},
     IResult,
 };
 
-/// Message ::= (Prefix Space)? Command CRLF
+/// A parsed message.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Message {
+    /// Optional Prefix, when forwarding messages from one client to another the server will add
+    /// a `Prefix` to show the origin of the message. Messages from client to server should not
+    /// contain a prefix.
     pub prefix: Option<Prefix>,
+    /// Command contained in the message, for example a [`Command::Message`] sent from a client to the server
+    /// will result in a message sent to all clients connected to the server.
     pub command: Command,
 }
 
+// TODO: Add more variants corresponding to the part of the message that caused the parsing error.
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParseMessageError {
+    All,
+}
+
+impl fmt::Display for ParseMessageError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ParseMessageError::*;
+        match self {
+            All => f.write_str("Failed to parse message"),
+        }
+    }
+}
+
+impl FromStr for Message {
+    type Err = ParseMessageError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // TODO: Improve error reporting
+        let (_, message) = parse_message(s).map_err(|_| ParseMessageError::All)?;
+
+        Ok(message)
+    }
+}
+
+// Message ::= (Prefix Space)? Command CRLF
 fn parse_message(input: &str) -> IResult<&str, Message> {
-    map(
-        pair(opt(terminated(parse_prefix, char(' '))), parse_command),
-        |(prefix, command)| Message { prefix, command },
-    )(input)
+    complete(terminated(
+        map(
+            pair(opt(terminated(parse_prefix, char(' '))), parse_command),
+            |(prefix, command)| Message { prefix, command },
+        ),
+        crlf,
+    ))(input)
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -35,7 +78,7 @@ pub struct Prefix {
     nick: String,
 }
 
-/// Prefix ::= ':' Nickname ;
+// Prefix ::= ':' Nickname ;
 fn parse_prefix(input: &str) -> IResult<&str, Prefix> {
     map(preceded(char(':'), alpha1), |nick: &str| Prefix {
         nick: nick.to_owned(),
@@ -59,7 +102,7 @@ mod tests {
 
     #[test]
     fn parse_message_works() {
-        let input = ":olly MSG :Hi!, how's it going?";
+        let input = ":olly MSG :Hi!, how's it going?\r\n";
         let expected = Message {
             prefix: Some(Prefix {
                 nick: "olly".to_owned(),
@@ -69,5 +112,18 @@ mod tests {
 
         let result = parse_message(input);
         assert_eq!(Ok(("", expected)), result);
+    }
+
+    #[test]
+    fn message_from_str() {
+        let input = "NICK olly\r\n";
+        let expected = Message {
+            prefix: None,
+            command: Command::Nick("olly".to_owned()),
+        };
+
+        let message = input.parse::<Message>();
+
+        assert_eq!(Ok(expected), message);
     }
 }
