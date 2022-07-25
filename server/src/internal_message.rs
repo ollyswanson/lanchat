6a536a2e26e3c9549fc98f06ce032d6a51ea52ee
Olly Swanson <olly.swanson95@gmail.com>
2022-07-25 15:07:43 +0100

    Document internal messages

--- server/src/internal_message.rs
+++ server/src/internal_message.rs
@@ -1,12 +1,21 @@
+//! Internal messages.
+//!
+//! This module defines the different types used for passing messages from a client connection to
+//! the main actor orchestrating the server.
 use std::net::SocketAddr;
 
 use protocol::message::LanChatMessage;
 use tokio::sync::oneshot::Sender;
 
+/// A type for sending messages from a connection to the main actor.
 #[derive(Debug)]
 pub struct InternalMessage {
+    /// The address of the connected client.
     pub addr: SocketAddr,
+    /// The message sent from the client to the server.
     pub msg: LanChatMessage,
+    /// The sending half of a oneshot channel used to send a `Response` from the server actor back
+    /// to the client once `msg` has been processed.
     pub respond: Sender<Response>,
 }
 
@@ -20,8 +29,12 @@ impl InternalMessage {
     }
 }
 
+/// A response that the main actor sends back to the task handling a connection.
 #[derive(Debug)]
 pub enum Response {
+    /// A straightforward acknowledgment that the command has been processed.
     Ack,
+    /// A command telling the connection task to hang up, is issues after the client has sent a
+    /// QUIT command.
     HangUp,
 }
